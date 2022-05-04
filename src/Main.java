a6216c5dfbf89dfce4ae81decd741389a8d5d604
Shreypatel911 <shreypatel11999@gmail.com>
2022-05-04 21:30:38 +0530

    add display method basic code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,44 @@
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
+    public static List<Student> studentList = new ArrayList<>();
     public static void main(String[] args) {
 
+        int input = 1;
+        while(true)
+        {
+            //Console console = System.console();
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.println("1.Add new student 2.Display all students 3.Exit");
+            input = scanner.nextInt();
+
+            if(input == 1) {
+                System.out.println("Enter rollnumber:");
+                int rollNumber = scanner.nextInt();
+                System.out.println("Enter name:");
+                String name = scanner.next();
+                System.out.println("Enter surname:");
+                String surname = scanner.next();
+                System.out.println("Enter standard:");
+                int std = scanner.nextInt();
+                System.out.println("Enter division:");
+                String div = scanner.next();
+
+                Student student = new Student(rollNumber, name, surname, std, div);
+                studentList.add(student);
+            }
+            else if(input == 2){
+                for(Student obj : studentList)
+                    System.out.println(obj.getName());
+            }
+            else
+                break;
+        }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,63 @@
 public class Student {
 
+    private int rollNumber;
+    private String name;
+    private String surname;
+    private int std;
+    private String div;
 
+    public Student() {
+    }
+
+    public Student(int rollNumber, String name, String surname, int std, String div) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.surname = surname;
+        this.std = std;
+        this.div = div;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getStd() {
+        return std;
+    }
+
+    public void setStd(int std) {
+        this.std = std;
+    }
+
+    public String getDiv() {
+        return div;
+    }
+
+    public void setDiv(String div) {
+        this.div = div;
+    }
+
+    public String getFullName() {
+        return name + " " + surname;
+    }
 }
